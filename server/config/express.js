b43c8efa859f31717669e21107f5da1f70f53483
dotcloudbxrn <a2tonev@gmail.com>
2017-12-09 13:41:48 +0100

    Added something like an interface and started styling it. Will be a long fight

--- server/config/express.js
+++ server/config/express.js
@@ -2,8 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 module.exports = (app) => {
-  app.set('view engine', 'handlebars')
   app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+  app.set('view engine', 'handlebars')
   app.use(express.static('public'))
   console.log('Express Ready.')
 }
