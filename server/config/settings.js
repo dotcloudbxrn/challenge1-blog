a52eeffe2d06476bf8da96d9b1e9c450a97990e4
dotcloudbxrn <a2tonev@gmail.com>
2017-12-09 12:16:44 +0100

    fixed db path for heroku

--- server/config/settings.js
+++ server/config/settings.js
@@ -2,10 +2,12 @@ const path = require('path')
 
 module.exports =  {
   development: {
-    rootDir: path.normalize(__dirname, '../..'),
-    dbPath: 'mongodb://localhost:27017/Blog',
-    port: 2525
+    rootDir: path.normalize(__dirname, '../../'),
+    dbPath: 'mongodb://localhost:27017/Blog'
   },
   staging: {},
-  production: {},
+  production: {
+    rootDir: path.normalize(__dirname,'../../'),
+    dbPath: process.env.MONGODB_URI
+  }
 }
