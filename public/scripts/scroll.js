ff6c4504923ecf2832bce37e150c723ba106702f
dotcloudbxrn <a2tonev@gmail.com>
2017-12-29 13:19:46 +0100

    trying to fix script loading and affecting phone users not being able to swipe left and right on user posts

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,9 +1,10 @@
 // USING https://github.com/jquery/jquery-mousewheel
 
-
-$(function() {
-  $('.user-posts').mousewheel(function(evt, chg) {
-     this.scrollLeft -= (chg * 50); //need a value to speed up the change
-     evt.preventDefault();
+if (!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch)) {
+  $(function() {
+    $('.user-posts').mousewheel(function(evt, chg) {
+       this.scrollLeft -= (chg * 50); //need a value to speed up the change
+       evt.preventDefault();
+    });
   });
-});
+}
